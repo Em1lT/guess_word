082166b0abbb83f2dbc967f97bfe2644aadbaecc
Emil Toivainen <emilanton.toivainen@gmail.com>
2022-01-21 16:55:06 +0000

    Feat: building works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use text_io::read;
 
 fn main() {
-    let list: [&str; 3] = ["dog", "cat", "cow"];
     println!("Guess a 3 letter word");
+
+    let list: [&str; 3] = ["dog", "cat", "cow"];
     let word: String = read!();
-    let corr: &str = if list.contains(&word as &str) { "correct" } else { "false" };
+    let corr: &str = if list.contains(&word.as_str()) { "correct" } else { "false" };
+
     println!("you got: {}", corr);
 }
