55172441d9905d5b75587a4184a358fb7473e568
Emil Toivainen <emilanton.toivainen@gmail.com>
2022-01-22 17:48:47 +0000

    Feat: random dependency

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use text_io::read;
+use rand::Rng;
 
 fn main() {
-    println!("Guess a 3 letter word");
-
     let list: [&str; 3] = ["dog", "cat", "cow"];
+    let mut random_number = rand::thread_rng();
+    let random_word = list[random_number.gen_range(0..list.len() - 1)];
+
+    println!("Guess a 3 letter word");
+    println!("? ? ?");
     let word: String = read!();
-    let corr: &str = if list.contains(&word.as_str()) { "correct" } else { "false" };
+
+    println!("{}", random_word);
+    let corr: &str = if random_word.eq(&word.as_str()) { "correct" } else { "false" };
 
     println!("you got: {}", corr);
+
 }
