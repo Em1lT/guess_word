8964d775f67059326a6636f40355f9b6ef6a4604
Emil Toivainen <emilanton.toivainen@gmail.com>
2022-01-22 19:48:51 +0000

    Feat: started doing the check logig

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,25 @@ fn main() {
     let list: [&str; 3] = ["dog", "cat", "cow"];
     let mut random_number = rand::thread_rng();
     let random_word = list[random_number.gen_range(0..list.len() - 1)];
+    let tries: u32 = 3;
 
-    println!("Guess a 3 letter word");
+    println!("Guess a 3 letter word, 3 tries");
     println!("? ? ?");
+
     let word: String = read!();
+    let corr: bool = random_word.eq(word.as_str());
 
-    println!("{}", random_word);
-    let corr: &str = if random_word.eq(&word.as_str()) { "correct" } else { "false" };
+    let nextGuess: String;
 
-    println!("you got: {}", corr);
-
+    if !corr {
+        for c in word.chars() {
+            if c == random_word.chars()[c] {
+                nextGuess += "O "
+            } else if random_word.chars().contains(c) {
+                nextGuess += "O "
+            };
+        };
+    } else {
+        println!("you got: {} on the {}", corr, tries);
+    };
 }
