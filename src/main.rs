2b4813f37f3f63e5c12213685021056c8b5160e8
Emil Toivainen <emilanton.toivainen@gmail.com>
2022-01-25 15:44:37 +0000

    Feat: working build

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 use text_io::read;
 use rand::Rng;
 
+fn ask_input()-> String {
+    let word: String = read!();
+    word
+}
+
 fn main() {
     let list: [&str; 3] = ["dog", "cat", "cow"];
     let mut random_number = rand::thread_rng();
     let random_word = list[random_number.gen_range(0..list.len() - 1)];
-    let tries: u32 = 3;
 
     println!("Guess a 3 letter word, 3 tries");
     println!("? ? ?");
 
-    let word: String = read!();
-    let corr: bool = random_word.eq(word.as_str());
+     let word: String = ask_input();
+
     let nextGuess: String;
 
     if !word.is_empty() {
         for (i, c) in word.chars().enumerate() {
-            let wordStr: &str = random_word.collect();
+            let wordStr: Vec<char> = random_word.chars().collect();
             println!("{}", c);
+            println!("{}", wordStr[i]);
+
             // if c == wordStr {
             //     nextGuess += "O "
             // } else if random_word.chars().contains(c) {
