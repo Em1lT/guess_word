9e9af981ab89d306f6f2ac8c59af391f17a60b2a
Emil Toivainen <emilanton.toivainen@gmail.com>
2022-01-25 16:28:06 +0000

    Feat: implemented 3 tries only

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use text_io::read;
 use rand::Rng;
 
 fn ask_input()-> String {
-    let word: String = read!();
-    word
+    read!()
 }
 
 fn main() {
@@ -11,16 +10,27 @@ fn main() {
     let mut random_number = rand::thread_rng();
     let random_word = list[random_number.gen_range(0..list.len() - 1)];
 
-    println!("Guess a 3 letter word, 3 tries");
-    println!("? ? ?");
+    println!(" [ Guess a 3 letter word, 3 tries ]");
+    println!(" [ ? ? ? ]");
+    let mut next_guess: String = "".to_owned();
+    let mut guess_correct: bool = false;
+    let mut tries: u8 = 0;
+    let total_tries: u8 = 3;
 
-     let word: String = ask_input();
-     let mut next_guess: String = "".to_owned();
+    while !guess_correct && tries != total_tries {
+        next_guess.clear();
+        let mut word: String = "".to_owned();
+        //while word.len() != 3 {
+            word.push_str(&ask_input());
+        // }
 
-
-    if !word.is_empty() {
         for (i, c) in word.chars().enumerate() {
             let word_str: Vec<char> = random_word.chars().collect();
+
+            if word == random_word {
+                guess_correct = true;
+                break
+            }
 
             if c == word_str[i] {
                 next_guess.push_str("O ")
@@ -30,8 +40,7 @@ fn main() {
                 next_guess.push_str("X ")
             }
         };
-        println!("{}", next_guess);
-    } else {
-        println!("No input");
-    };
+        println!("[ {}]", next_guess);
+        tries = tries + 1;
+    }
 }
