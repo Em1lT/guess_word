091908b2370371de0d89e4df6f8239b2bfff0a4d
Emil Toivainen <emilanton.toivainen@gmail.com>
2022-01-26 19:31:51 +0000

    Feat: breaking down the code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use text_io::read;
 use rand::Rng;
-use std::io::{self, Write};
+// use std::io::{self, Write};
 
 fn ask_input()-> String {
     read!()
+}
+
+fn enumarate_answer(guess_word: String, correct_word: &str)-> String {
+    let mut next_guess: String = "".to_owned();
+    let word_str: Vec<char> = correct_word.chars().collect();
+
+    for (i, c) in guess_word.chars().enumerate() {
+        if c == word_str[i] {
+            next_guess.push_str("O ")
+        } else if correct_word.contains(c) {
+            next_guess.push_str("! ")
+        } else {
+            next_guess.push_str("X ")
+        }
+    };
+
+    next_guess
 }
 
 fn main() {
@@ -13,13 +30,11 @@ fn main() {
 
     println!(" [ Guess a 3 letter word, 3 tries ]");
     println!(" [ ? ? ? ]");
-    let mut next_guess: String = "".to_owned();
     let mut guess_correct: bool = false;
     let mut tries: u8 = 0;
     let total_tries: u8 = 3;
 
     while !guess_correct && tries != total_tries {
-        next_guess.clear();
         let mut word: String = "".to_owned();
 
         while word.len() != 3 {
@@ -31,22 +46,12 @@ fn main() {
             }
         }
 
-        for (i, c) in word.chars().enumerate() {
-            let word_str: Vec<char> = random_word.chars().collect();
+        if word == random_word {
+            guess_correct = true;
+        }
+        let answer_row: String = enumarate_answer(word, random_word);
 
-            if word == random_word {
-                guess_correct = true;
-            }
-
-            if c == word_str[i] {
-                next_guess.push_str("O ")
-            } else if random_word.contains(c) {
-                next_guess.push_str("! ")
-            } else {
-                next_guess.push_str("X ")
-            }
-        };
-        println!("[ {}]", next_guess);
+        println!("[ {}]", answer_row);
         tries = tries + 1;
     }
     let msg = if guess_correct { "You won!" } else { "You lost!" };
